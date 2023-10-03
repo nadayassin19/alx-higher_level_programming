454e12247eb2dc2b0efa313a4033085be400c448
nadayassin19 <yassinada19@gmail.com>
2023-10-04 01:15:14 +0300

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(ch):
+    for i in range(len(ch)):
+        asc = ord(ch[i])
+        if asc > 96 and asc < 123:
+            num = 32
+        else:
+            num = 0
+        print("{0:c}".format(asc - num), end="")
