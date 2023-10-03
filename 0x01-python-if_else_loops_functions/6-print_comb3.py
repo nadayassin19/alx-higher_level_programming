e151a2f66d9a9f803884e39ef9ae9ed087f467c0
nadayassin19 <yassinada19@gmail.com>
2023-10-04 00:36:52 +0300

    task6 edit0
    pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 90):   
-    if i % 10 > i/ 10:
+for i in range(0, 90):
+    if i % 10 > i / 10:
         if i != 89:
             print("{0:02d}, ".format(i), end="")
         else:
