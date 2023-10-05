51d2fc823a897068525d3db1fb8bed25db59852c
nadayassin19 <yassinada19@gmail.com>
2023-10-05 22:27:47 +0300

    advanced task3 edit1

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("\d65\d66\d67\d68\d69\d70\d71\d72\d73\d74\d75\d76",end="")
-print("\d77\d78\d79\d80\d81\d82\d83\d84\d85\d89\d90")
+print(u"\d65\d66\d67\d68\d69\d70\d71\d72\d73\d74\d75\d76",end="")
+print(u"\d77\d78\d79\d80\d81\d82\d83\d84\d85\d89\d90")
