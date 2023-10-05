a0874b2e2b7bfbd78cce7808e704444bbc52a178
nadayassin19 <yassinada19@gmail.com>
2023-10-05 20:24:03 +0300

    task2 edit1
    edit the if condition to print the passed arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     elif i == 1:
         print("1 argument")
     else:
-        print("{} argument".format(i))
+        print("{} arguments".format(i))
     if i >= 1:
         i = 0
-        for arg in range(len(sys.argv)):
+        for arg in sys.argv:
             if i != 0:
                 print("{}: {}".format(i, arg))
             i = i + 1
