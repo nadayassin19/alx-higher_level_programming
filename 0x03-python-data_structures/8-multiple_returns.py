d5ed81775455669f4273979f19eb35f37af34d31
Nada Yassin <yassinada19@gmail.com>
2023-10-08 14:34:43 -0700

    task8 edit0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,9 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         sentence[0] = None
-        #req_tuple = ("{:c}", "{:c}".format(length, sentence[0]))
         req_tuple = (length, sentence[0])
     else:
-        #req_tuple = ("{:c}", "{:c}".format(length, sentence[0]))
         req_tuple = (length, sentence[0])
     return (req_tuple)
