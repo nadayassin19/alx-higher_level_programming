8febd414341758d2f77baedc14f15b81a43c9a44
nadayassin19 <yassinada19@gmail.com>
2023-10-09 01:38:38 +0300

    task11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    length = len(my_list)
+    new_list = []
+    if idx < 0 or idx > (length - 1):
+        return(my_list)
+    else:
+        for i in range (length):
+            if i != idx:
+                new_list.append(my_list[i])
+        return (new_list)
