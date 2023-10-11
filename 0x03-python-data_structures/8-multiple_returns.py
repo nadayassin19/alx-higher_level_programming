a8d1ddacd4aef9957637b8dd66b31e42f26a5fdf
nadayassin19 <yassinada19@gmail.com>
2023-10-11 15:48:41 +0300

    task8 edit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        sentence[0] = None
-        req_tuple = (length, sentence[0])
+        req_tuple = (length, None)
     else:
         req_tuple = (length, sentence[0])
     return (req_tuple)
