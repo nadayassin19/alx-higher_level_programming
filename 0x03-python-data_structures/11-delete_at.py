54191ac2931ab44c2e0940326223085720fb529a
nadayassin19 <yassinada19@gmail.com>
2023-10-11 16:28:30 +0300

    task11
    adding a comment

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -8,5 +8,6 @@ def delete_at(my_list=[], idx=0):
         for i in range(length):
             if i != idx:
                 new_list.append(my_list[i])
+        #trying to return the new value of my_list after deleting an element but it doesn't work
         my_list = new_list
         return (new_list)
