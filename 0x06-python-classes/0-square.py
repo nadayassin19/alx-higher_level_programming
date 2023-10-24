7897125a15d3ae16419d2440f6326dd55a6e5fd0
nadayassin19 <yassinada19@gmail.com>
2023-10-24 22:38:32 +0300

    task1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr//bin/python3
 class Square:
-    """It's a class that defines a square"""
+    """It's a class that defines a square.
+    """
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """It's a class that defines a square by size
+    """
+    def __init__(self, size):
+        """initialize size
+
+        Args:
+            size (int): square size.
+        """
+        self.__size = size
+        
