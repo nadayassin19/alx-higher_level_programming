d61161c40a4d42a069f67084c005aa6096bdd98b
nadayassin19 <yassinada19@gmail.com>
2023-10-26 01:03:25 +0300

    task9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a suare module that defines a suare by its size & get its area"""
+
+
+class Square:
+    """it's a class that defines a square
+    """
+    def __init__(self, size=0):
+        """initialize size
+
+        Args:
+            size (int): square size.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """to retrieve the size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """to set the size value
+
+        Args:
+            value (int): size value
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """square area
+        """
+        return (self.__size ** 2)
