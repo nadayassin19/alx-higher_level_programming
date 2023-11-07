45fe88280845279ec3d121710af5e87e9c343fea
nadayassin19 <yassinada19@gmail.com>
2023-11-07 20:58:40 +0200

    task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a module of a function that writes a string
+to text file
+"""
+
+
+def write_file(filename="", text=""):
+    """it's a function that writes a string
+    to a text file (UTF8) and returns
+    the number of characters written.
+
+    Args:
+        filename (str): given file name.
+        text (str): the text to be written.
+    """
+    lines = 0
+    with open(filename, 'w', encoding="utf-8") as f:
+        for l in f:
+            lines += 1
+        return (lines)
