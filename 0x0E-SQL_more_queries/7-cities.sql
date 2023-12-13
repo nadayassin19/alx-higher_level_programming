2c61ae50a8ca43e051acd297c73cd0f0024b1aca
nadayassin19 <yassinada19@gmail.com>
2023-12-13 16:49:16 +0200

    EDIT0 TASK7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,8 +4,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES states(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    state_id INOT NULL
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL
 );
