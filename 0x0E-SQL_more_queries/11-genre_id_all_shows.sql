6baa99bf0505f1d08ce79f9c1a3f31dd960a1240
nadayassin19 <yassinada19@gmail.com>
2023-12-13 17:18:25 +0200

    EDIT0 TASK11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- 11. Genre ID for all shows
 -- A script that lists all shows contained in a database.
 
+SELECT s.title, g.genre_id
 FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
 ON s.id = g.show_id
-ORDER BY s.title, g.genre_id
-SELECT s.title, g.genre_id;
+ORDER BY s.title, g.genre_id;
