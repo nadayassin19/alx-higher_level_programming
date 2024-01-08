0737ced2f66ce7c1b467d50da810121dc368aea6
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-08 23:19:36 +0200

    task7 edit0

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
 if (Number.isNaN(x)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
-else {for (let i = 0; i < x; i++) {
-  console.log('C is fun');
+else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
 }}
