1fa6c58388f4b2485acd7abf9fdceb98247a23a8
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-08 20:51:14 +0200

    first 3 tasks

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv.length;
+if (arg == 0){
+    console.log('No argument');
+}
+else if (arg == 1){
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found');
+}
