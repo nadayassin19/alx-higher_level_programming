44a89377669aef3e924b709df41c1319947a2547
Nada Yassin <yassinada19@gmail.com>
2024-01-08 13:29:30 -0800

    task7 edit3

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 const x = parseInt(process.argv[2]);
 if (Number.isNaN(x)) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
-}}
+  }
+}
