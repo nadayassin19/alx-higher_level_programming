e299cbb3227679435339679db7472fc212e9b981
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-08 23:56:06 +0200

    task9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[4]);
+if (Number.isNaN(a) || Number.isNaN(b)) {
+  console.log(NaN)
+}
+else {
+  const c = a + b;
+  console.log(c);
+}
