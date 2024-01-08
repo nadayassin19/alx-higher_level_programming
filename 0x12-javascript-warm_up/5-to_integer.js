be67081d646aef67d76798e9d18f6f3530a95e79
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-08 22:52:08 +0200

    task5 edit2

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const myArg = parseInt(process.argv[2]);
-console.log(myArg === NaN ? 'Not a number' : 'My number: ' + myArg);
+console.log(Number.isNaN(myArg) ? 'Not a number' : 'My number: ' + myArg);
