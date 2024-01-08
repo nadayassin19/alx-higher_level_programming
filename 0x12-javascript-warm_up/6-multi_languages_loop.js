465a178d206ec168116c31e78a75e69113189e22
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-08 23:02:39 +0200

    task6 edit1

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const i = 0; i < 3; i++) {
-    console.log(myArray[i]);
+for (let i = 0; i < 3; i++) {
+  console.log(myArray[i]);
 }
