757fb2d654a64de06d47820ab88c4545d1bd186a
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-08 22:01:40 +0200

    task2 edit1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const myArg = process.argv.length;
-console.log(myArg > 3 ? 'No argument' : myArg === 3 ? 'Argument found' : 'Arguments found');
+console.log(myArg < 3 ? 'No argument' : myArg === 3 ? 'Argument found' : 'Arguments found');
