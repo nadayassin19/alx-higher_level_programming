1a4903811d48d1125c6d9c2f48dde864472509fa
Nada Yassin <yassinada19@gmail.com>
2024-01-08 13:48:08 -0800

    task8 edit2

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,11 @@ const x = parseInt(process.argv[2]);
 if (Number.isNaN(x)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0, s; i < x; i++) {
+    s = '';
     for (let j = 0; j < x; j++) {
-      console.log('X');
+      s += 'X';
     }
+    console.log(s);
   }
 }
