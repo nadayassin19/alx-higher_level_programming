855487b4793ffedf1271784d5389036a91ea95f4
Nada Yassin <yassinada19@gmail.com>
2024-01-08 11:28:09 -0800

    edit permission of files & run files

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const arg = process.argv.length;
-if (arg == 0){
-    console.log('No argument');
-}
-else if (arg == 1){
-    console.log('Argument found');
-}
-else {
-    console.log('Arguments found');
-}
+const myArg = process.argv.length;
+console.log(myArg == 1 ? 'Argument found' : myArg > 1 ? 'Arguments found' : 'No argument');
