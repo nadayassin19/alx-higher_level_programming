938152b4635a583ab38186be1114b020977abc0b
nadayassin19 <87150606+nadayassin19@users.noreply.github.com>
2024-01-09 22:40:20 +0200

    first 6 tasks

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0, s; i < this.width; i++) {
+      s = '';
+      for (let j = 0; j < this.height; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Rectangle;
