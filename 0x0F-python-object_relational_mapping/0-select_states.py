2eb76893daf16c7247ad6cac95f5daae45fd5162
nadayassin19 <yassinada19@gmail.com>
2024-02-20 03:09:05 +0200

    first 5 tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"It's a module that  lists all states from the database hbtn_0e_0_usa"
+"""  A module that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-
-if __name == "__main__":
-    db = MySLdb.
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
